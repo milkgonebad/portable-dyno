ae24dc24a46210c0d06a9efe7657ef0672643104
Amy Black <amy@gothicmaine.com>
2012-10-31 22:56:49 -0400

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortableDyno::Application.config.secret_token = '971ee6eaf9138a06a61948d6eed756eb8ea931aa10b184ecc4f48624b1fd7530d5abbd659e6ea87e8a30ec288272066ea4329219096b603bddca0548a464d4ee'
