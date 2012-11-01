2243cdece327a39c4cbb52ce29da92599dc27e05
Amy Black <amy@gothicmaine.com>
2012-11-01 19:44:14 -0400

    added devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ PortableDyno::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  config.assets.initialize_on_precompile = false
+  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
