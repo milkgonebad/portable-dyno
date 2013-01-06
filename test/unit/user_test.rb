96d493c6b51c359ac9bfd931115a076e942fe2d5
Amy Black <amy@gothicmaine.com>
2013-01-06 16:55:44 -0500

    Override devise allow deleting oneself
    Override allowing a user to delete themself and cleanup

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  #TODO:  add tests for users when we do anything more than what devise supplies
+  
 end
