e1e7d8a22f43c37843e1356327e358ad29307df9
Amy Black <amy@gothicmaine.com>
2013-01-06 14:47:44 -0500

    converted static pages to rails

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
+  
   def index
+#    @events = Event.future_published_events
+    render :layout => 'home_index'
   end
+  
+  def contact
+    @marquee_title = 'Schedule an Event'
+  end
+  
+  def faq
+    @marquee_title = 'Frequently Asked Questions'
+  end
+  
+  def about
+    @marquee_title = 'About Our Dyno'
+  end
+  
+  def gallery
+    @marquee_title = 'Gallery'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,22 @@
 PortableDyno::Application.routes.draw do
 
-  get "events/index"
-
-  get "events/new"
-
-  get "events/create"
-
-  get "events/edit"
-
-  get "events/update"
-
-  get "events/destroy"
-
+  # public routes
   root :to => 'home#index'
+  get '/contact' => 'home#contact'
+  get '/faq' => 'home#faq'
+  get '/about' => 'home#about'
+  get '/gallery' => 'home#gallery'
+  
+  # admin only routes
+  match 'admin' => 'admin#index'
   devise_for :users, :skip => [:registrations]
     as :user do # only use the registration routes we need
       get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
       put 'users' => 'devise/registrations#update', :as => 'user_registration'
   end
-  scope "/admin" do # scope the user admin pages so they don't conflict with devise
+  scope '/admin' do # scope the user admin pages so they don't conflict with devise
     resources :users
   end
-  match "admin" => 'admin#index'
   resources :events
 
 end
